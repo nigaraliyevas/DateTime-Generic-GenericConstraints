1cef7464151e23d3b2fb5abb0776b348dfad9b06
nigaraliyevas <naliyeva965@gmail.com>
2023-12-25 02:31:47 +0400

    initial

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*DateTime tarix = new DateTime(2002, 10, 4);
+Console.WriteLine(tarix.TimeOfDay);*/
+using DateTimeAndGeneric_Non_Generic;
+
+/*MyList<int> myIntList = new();
+myIntList.Name = 1; ;
+
+MyList<int[]> myIntArrList = new();
+Array.Sort();
+*/
+List<int> list = new ();
+list.Add (1);
+list.Add((int)15);
+list.Add (2);
+list.Add (3);
+list.Add (4);
+list.Add (5);
+list[5] = 12;
+foreach (int i in list)
+{
+    Console.WriteLine(i);
+}
+
+
+
---
+++ User.cs
@@ -0,0 +1,8 @@
+﻿namespace DateTimeAndGeneric_Non_Generic
+{
+    public class User<T>
+    {
+        public int Id { get; set; }
+        public T Point { get; set; }
+    }
+}
